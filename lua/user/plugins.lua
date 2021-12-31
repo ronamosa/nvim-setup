4aee7f83705acc0f89cf9b29a062cff0b3c19cb8
Ron <ron@cloudbuilder.io>
2021-12-31 22:08:58 +1300

    add git plugins

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -94,6 +94,8 @@ return packer.startup(function(use)
 
   -- Git
   use "lewis6991/gitsigns.nvim"
+  use "https://tpope.io/vim/fugitive.git"
+  use "https://tpope.io/vim/rhubarb.git"
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
